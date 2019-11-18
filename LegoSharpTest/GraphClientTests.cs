80257ff267dda2a41ef9bd5f8ef431ab6c356296
rolledback <matthew.rayermann@gmail.com>
2019-11-17 20:21:39 -0800

    Add support for all colors

--- LegoSharpTest/GraphClientTests.cs
+++ LegoSharpTest/GraphClientTests.cs
@@ -31,5 +31,31 @@ namespace LegoSharpTest
                 Assert.IsTrue(!string.IsNullOrEmpty(brick.id));
             }
         }
+
+        [TestMethod]
+        public async Task allColorsValid()
+        {
+            LegoGraphClient graphClient = new LegoGraphClient();
+            await graphClient.authenticateAsync();
+
+            var colors = (LegoColor[])Enum.GetValues(typeof(LegoColor));
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                LegoColor currColor = colors[i];
+                string currColorString = ColorFilter.colorToString(currColor);
+
+                LegoGraphSearch graphSearch = new LegoGraphSearch();
+                graphSearch.addFilter(new ColorFilter()
+                    .addValue(currColor)
+                );
+
+                IEnumerable<Brick> result = await graphClient.searchForBricksAsync(graphSearch);
+                foreach (Brick brick in result)
+                {
+                    Assert.IsTrue(brick.variant.attributes.colour.Equals(currColorString));
+                }
+            }
+        }
     }
 }
