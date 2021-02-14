e75146e0f1c63bfb963d0750217c67179a99248b
Matthew Rayermann <matthew.rayermann@gmail.com>
2021-02-14 13:13:17 -0800

    Fix scraper to scrape pick a brick facets correctly

--- Scraper/Scraper.cs
+++ Scraper/Scraper.cs
@@ -21,7 +21,7 @@ namespace Scraper
 
             Console.WriteLine("\n-----------------------------------------\n");
 
-            foreach (var entry in await (new FacetScraper<ProductSearchQuery, ProductSearchResult>(new List<ProductSearchQuery> { new ProductSearchQuery() }, new ProductSearchFacetExtractor())).scrapeFacets())
+            foreach (var entry in await (new FacetScraper<PickABrickQuery, PickABrickResult>(new List<PickABrickQuery> { new PickABrickQuery() }, new PickABrickFacetExtractor())).scrapeFacets())
             {
                 Console.WriteLine(entry.Key);
                 foreach (var label in entry.Value)
